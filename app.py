1f054f1185b251bfdf59c1a95a26053f93b8e9ca
yacoop <groobzone@gmail.com>
2022-12-01 14:57:45 +0100

    orginizing code and changing theme

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from main_window import *
+import qdarkstyle
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='PySide6'))
     window.show()
     sys.exit(app.exec())
--- main_window.py
+++ main_window.py
@@ -13,76 +13,90 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+
+        self.setWindowIcon(QIcon('icons/app_icon.png'))
         self.setWindowTitle("EKG")
         self.resize(800, 600)
 
-
         self.x = []
         self.ecg = []
-        # self.ecg_cleaned = []
-        # Create a menu bar
-        self.menuBar = self.menuBar()
+
+        self.ecg_peaks_methods = ["neurokit", "pantompkins1985", "hamilton2002", "elgendi2010", "engzeemod2012", "gamboa2008"]
+        self.ecg_clean_methods = ["neurokit", "biosppy", "pantompkins1985", "hamilton2002", "elgendi2010", "engzeemod2012"]        
         
+        def create_menuBar():
+            def open():
+                self.x = np.arange(0,15000)
+                self.ecg = nk.ecg_simulate(duration = 15, sampling_rate = 1000, heart_rate = 80)
+                ecg_line.setData(self.x, self.ecg)
 
-        def open():
-            self.x = np.arange(0,15000)
-            self.ecg = nk.ecg_simulate(duration = 15, sampling_rate = 1000, heart_rate = 80)
-            ecg_line.setData(self.x, self.ecg)
+            def save():
+                print('clicked save')
 
-        def save():
-            print('clicked save')
+            def help():
+                print('clicked help')
 
-        def help():
-            print('clicked help')
+            def quit():
+                QApplication.quit()
 
-        def quit():
-            QApplication.quit()
+
+            self.menuBar = self.menuBar()
+
+            open_action = QAction('Open', self)
+            open_action.triggered.connect(lambda:open())
+            self.menuBar.addAction(open_action)
+
+            save_action = QAction('Save', self)
+            save_action.triggered.connect(lambda:save())
+            self.menuBar.addAction(save_action)
+
+            help_action = QAction('Help', self)
+            help_action.triggered.connect(lambda:help())
+            self.menuBar.addAction(help_action)
+
+            quit_action = QAction('&Quit', self)
+            quit_action.triggered.connect(lambda:quit())
+            self.menuBar.addAction(quit_action)
 
         def ecg_clean_f(i):
             if len(self.x) == 0:
                 print('open file')
-            elif i == 0:
-                ecg_cleaned = nk.ecg_clean(self.ecg, 1000, 'neurokit')
+            else:
+                ecg_cleaned = nk.ecg_clean(self.ecg, 1000, self.ecg_clean_methods[i])
                 ecg_line.setData(self.x, ecg_cleaned)
-            elif i == 1:
-                ecg_cleaned = nk.ecg_clean(self.ecg, 1000, 'biosppy')
-                ecg_line.setData(self.x, ecg_cleaned)
-            elif i == 2:
-                ecg_cleaned = nk.ecg_clean(self.ecg, 1000, 'pantompkins1985')
-                ecg_line.setData(self.x, ecg_cleaned)
-            elif i == 3:
-                ecg_cleaned = nk.ecg_clean(self.ecg, 1000, 'hamilton2002')
-                ecg_line.setData(self.x, ecg_cleaned)
-            elif i == 4:
-                ecg_cleaned = nk.ecg_clean(self.ecg, 1000, 'elgendi2010')
-                ecg_line.setData(self.x, ecg_cleaned)
-            elif i == 5:
-                ecg_cleaned = nk.ecg_clean(self.ecg, 1000, 'engzeemod2012')
-                ecg_line.setData(self.x, ecg_cleaned)
+
+        def create_ecg_clean_combo():
+            ecg_clean_label = QLabel("Clean method")
+            self.comboLayout.addWidget(ecg_clean_label)
+
+            ecg_clean =  QComboBox()
+           
+            for method in self.ecg_clean_methods:
+                ecg_clean.addItem(method) 
+
+            ecg_clean.activated[int].connect(ecg_clean_f)   
+            self.comboLayout.addWidget(ecg_clean)
 
         def ecg_peaks_f(i):
-            signals, info = nk.ecg_peaks(self.ecg, method = 'neurokit', correct_artifacts = True)
-            for peaks in info["ECG_R_Peaks"]:
-                self.graphWidget.addItem(pg.InfiniteLine(peaks, pen = pg.mkPen(color=(255, 0, 0))))
+            if len(self.x) == 0:
+                print('open file')
+            else:
+                s, info = nk.ecg_peaks(self.ecg, method = 'neurokit', correct_artifacts = True)
+                for peaks in info["ECG_R_Peaks"]:
+                    self.graphWidget.addItem(pg.InfiniteLine(peaks, pen = pg.mkPen(color=(255, 0, 0))))
 
+        def create_ecg_peaks_combo():
+            ecg_peak_label = QLabel("Find peak method")
+            self.comboLayout.addWidget(ecg_peak_label)
 
+            ecg_peaks = QComboBox()
+            for method in self.ecg_peaks_methods:
+                ecg_peaks.addItem(method)
 
+            self.comboLayout.addWidget(ecg_peaks)
+            ecg_peaks.activated[int].connect(ecg_peaks_f)   
 
-        open_action = QAction('Open', self)
-        open_action.triggered.connect(lambda:open())
-        self.menuBar.addAction(open_action)
-
-        save_action = QAction('Save', self)
-        save_action.triggered.connect(lambda:save())
-        self.menuBar.addAction(save_action)
-
-        help_action = QAction('Help', self)
-        help_action.triggered.connect(lambda:help())
-        self.menuBar.addAction(help_action)
-
-        quit_action = QAction('&Quit', self)
-        quit_action.triggered.connect(lambda:quit())
-        self.menuBar.addAction(quit_action)
+        create_menuBar()
 
         self.centralWidget = QWidget()
 
@@ -91,38 +105,28 @@ class MainWindow(QMainWindow):
         self.gridLayout = QGridLayout()
         self.centralWidget.setLayout(self.gridLayout)
 
-        self.graphWidget = pg.PlotWidget(background='w') #setting graph
+        self.graphWidget = pg.PlotWidget(background=QColor(69, 83, 100, 255)) #setting graph
 
         ecg_line = self.graphWidget.plot(self.x, self.ecg)
 
         self.gridLayout.addWidget(self.graphWidget, 0, 0) 
 
         self.comboLayout = QVBoxLayout()
+        self.comboLayout.addStretch()
+   
+
         self.gridLayout.addLayout(self.comboLayout, 0, 1)
+        
+        create_ecg_clean_combo()
 
-        self.ecg_clean = QComboBox()
-        self.ecg_clean.addItem("nuerokit")       
-        self.ecg_clean.addItem("biosppy")  
-        self.ecg_clean.addItem("pantompkin1985")
-        self.ecg_clean.addItem("hamilton2002")
-        self.ecg_clean.addItem("elgendi2010")
-        self.ecg_clean.addItem("engzeemod2012")      
+        self.comboLayout.addStretch()
 
-        self.ecg_clean.activated[int].connect(ecg_clean_f)   
+        create_ecg_peaks_combo()
 
-        self.comboLayout.addWidget(self.ecg_clean)
+        self.comboLayout.addStretch()
+        self.comboLayout.addStretch()
+        self.comboLayout.addStretch()
 
-        self.ecg_peaks = QComboBox()
-        self.comboLayout.addWidget(self.ecg_peaks)
-        self.ecg_peaks.addItem("neurokit")
-        # self.ecg_peaks.addItem("biosppy")  
-        # self.ecg_peaks.addItem("pantompkin1985")
-        # self.ecg_peaks.addItem("hamilton2002")
-        # self.ecg_peaks.addItem("elgendi2010")
-        # self.ecg_peaks.addItem("engzeemod2012")
-
-        self.ecg_peaks.activated[int].connect(ecg_peaks_f)   
-
-        self.button = QPushButton("SAVE")
-        self.button.clicked.connect(lambda:save())
-        self.gridLayout.addWidget(self.button, 1, 1)
+        # self.button = QPushButton("SAVE")
+        # self.button.clicked.connect(save())
+        # self.gridLayout.addWidget(self.button, 1, 1)
